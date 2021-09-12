965cd18075481fd9ac7a06c60a03c5940affd42c
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-09-11 21:43:24 -0400

    Create variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,68 +3,35 @@
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz" ;
 var numbers = "0123456789";
-var symbolChar = "!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
+var specialChar = "!@#$%^&*()_+~\\`|}{[]:;?><,./-=";
+var choices = ""
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 
-
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
  
-
+  
   var passwordText = document.querySelector("#password");
 
-  // var lengthQuestion = window.prompt ("How many characters would you like your password to be?")
-  // if (lengthQuestion === "5") {
-  //   passwordText.textContent = lengthQuestion;
-  // }
-
-  var upperCaseQuestion = window.prompt ("Would you like to use upper case letters? Type 'yes' or 'no'.")
-
-  upperCaseQuestion = upperCaseQuestion.toLowerCase();
-  if (upperCaseQuestion === "yes") {
-    for (let i = 0; i < 20; i++) {
-      let generator = upperCase[Math.floor(Math.random() * 26)];
-      
-      passwordText.textContent = generator
-      
-
-  }
- 
-    }
+  var lengthQuestion = window.prompt ("How many characters would you like your password to be?")
+  if (lengthQuestion === "5") {
+    passwordText.textContent = lengthQuestion;
   }
 
-    // passwordText.textContent = upperCase[Math.floor(Math.random()* 20)];
-    //   passwordText += writePassword;
-  
-  // function makePassword() {
-  //   passwordText.textContent = ("okay");
-  
-  // }
- 
-
-  // var password = generatePassword();
-
-  // var specialChar = \"" !"#$%&'()*+,-./:;<=>?@[\]_`{|}~^\";
 
 
 
 
-  // alert ("Whats up");
-
-  // passwordText.value = password;
-
-  // var passwordLength = window.alert ('How long would you like your password to be?')
 
 
 
-// Add event listener to generate button
 
-// generateBtn.addEventListener("click", writePassword);
 
-// generateBtn.addEventListener("click", function() {alert ("Hello world");});
+
 
